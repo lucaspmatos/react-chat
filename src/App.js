a4f0105f4234641e1ea55f25c9bb51227f002932
Lucas Matos <lucaspmatos@outlook.com>
2021-02-27 16:02:45 -0300

    Structuring Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import "./App.css";
+
+export default () => {
+  return <div className="app-window">
+    <div className="sidebar">
+      ...
+    </div>
+    <div className="content-area">
+      ...
+    </div>
+  </div>
+};
